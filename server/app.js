b8c7ecca56ec7691e3ead900465630085d7a6453
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-07-14 11:46:30 +0530

    second changes done

--- server/app.js
+++ server/app.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 const userRoutes = require("./routes/userRoutes");
 const connectToDB = require("./config/db");
+const cors = require("cors");
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
 connectToDB();
 
